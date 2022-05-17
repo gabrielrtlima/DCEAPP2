68d3352422f7cc328b056f8bb4169b3698423724
Rafael Levi <rafael.levi@d3.do>
2022-05-17 18:56:39 -0300

    aumentando tamanho do card

--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import './index.css';
 import Header from '../../components/Header';
+import { Grid, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -7,16 +9,29 @@ const Home = () => {
             <div className="home-body">
                 <Header />
                 <div className="home-container">
+                    <h1>Bem Vindo!</h1>
                     <div className="home-card-container">
-                        {/* <div className="home-img">
-                        </div> */}
-                        <img srcSet="./logo.png"/>
-                        <div className="home-texto">
-                            <p>
-                                DOAGUS é um sistema criado para intermediar a comunicação entre pessoas que desejam doar móveis em Garanhuns. A ideia surgiu de um projeto na Faculdade UPE, desenvolvido por alunos do curso de Engenharia de Software.
-                            </p>
-                        </div>
+                        <Grid container>
+                            <Grid item xs={6}>
+                                <img srcSet="./logo.png" />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <div className="home-texto">
+                                    <p>
+                                        DOAGUS é um sistema criado para intermediar a comunicação entre pessoas que desejam doar móveis em Garanhuns. A ideia surgiu de um projeto na Faculdade UPE, desenvolvido por alunos do curso de Engenharia de Software.
+                                    </p>
+                                </div>
+                            </Grid>
+                        </Grid>
                     </div>
+                    <Link to="/login" style={{ textDecoration: 'none' }}>
+                        <Button
+                            variant="contained"
+                            sx={{ marginTop: 2, fontSize: 20 }}
+                        >
+                            Login / Registrar
+                        </Button>
+                    </Link>
                 </div>
             </div>
         </>
