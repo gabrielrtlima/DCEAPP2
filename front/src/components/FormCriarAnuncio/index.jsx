1d402a6fbeb761991802d5aacb64c56ff02d4e80
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-30 20:35:27 -0300

    adicionando cadastro funcional

--- front/src/components/FormCriarAnuncio/index.jsx
+++ front/src/components/FormCriarAnuncio/index.jsx
@@ -1,12 +1,87 @@
 import React, { useState, useEffect } from 'react'
 import { Button, TextField, MenuItem, Select, InputLabel, FormControl } from '@mui/material'
 import './index.css'
+import { useNavigate } from 'react-router-dom'
 
 const FormCriarAnuncio = () => {
-    const [categoria, setCategoria] = useState('')
+    const usuarioId = localStorage.getItem('usuarioId')
+    const mudarRota = useNavigate()
 
-    const handleChangeCategoria = (e) => {
-        setCategoria(e.target.value)
+    const [dadosAnuncio, setDadosAnuncio] = useState({
+        nome: '',
+        descricao: '',
+        imagem: '',
+        categoria: '',
+        contato: '',
+        usuario_id: usuarioId
+    })
+
+    const handleNome = (e) => {
+        e.preventDefault()
+        setDadosAnuncio({
+            ...dadosAnuncio,
+            nome: e.target.value
+        })
+    }
+
+    const handleDescricao = (e) => {
+        e.preventDefault()
+        setDadosAnuncio({
+            ...dadosAnuncio,
+            descricao: e.target.value
+        })
+    }
+
+    const handleImagem = (e) => {
+        e.preventDefault()
+        const formData = new FormData();
+        formData.append("file", e.target.files[0]);
+        formData.append("upload_preset", "r2kismvj")
+        console.log(formData)
+        fetch("https://api.cloudinary.com/v1_1/dgkyahn55/image/upload", {
+            method: "POST",
+            body: formData
+        })
+        .then(res => res.json())
+        .then(data => {
+            setDadosAnuncio({
+                ...dadosAnuncio,
+                imagem: data.url
+            })
+        })
+
+    }
+
+    const handleCategoria = (e) => {
+        e.preventDefault()
+        setDadosAnuncio({
+            ...dadosAnuncio,
+            categoria: e.target.value
+        })
+    }
+
+    const handleContato = (e) => {
+        e.preventDefault()
+        setDadosAnuncio({
+            ...dadosAnuncio,
+            contato: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const response = await fetch('http://localhost:8080/api/anuncio', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dadosAnuncio)
+        });
+        const data = await response.json();
+        console.log(data);
+        
+        mudarRota("/anuncios");
     }
 
     return(
@@ -18,7 +93,7 @@ const FormCriarAnuncio = () => {
                     </div>
                     <div className="container-criar-anuncio">
                         <div className="criar-anuncio-img">
-                            <img src="./img-anuncio.svg" alt=""/>
+                            <img src={dadosAnuncio.imagem == "" ? "./img-anuncio.svg" : dadosAnuncio.imagem} alt=""/>
                             <Button
                             variant="contained"
                             component="label"
@@ -28,21 +103,22 @@ const FormCriarAnuncio = () => {
                                     type="file"
                                     hidden
                                     required
+                                    onChange={handleImagem}
                                 />
                             </Button>
                         </div>
                         <form className="criar-anuncio-form">
-                            <TextField id="standard-basic" label="Nome do anúncio" variant="standard" required />
-                            <TextField id="standard-basic" label="Descrição" variant="standard" multiline required/>
+                            <TextField id="standard-basic" label="Nome do anúncio" variant="standard" required onChange={handleNome}/>
+                            <TextField id="standard-basic" label="Descrição" variant="standard" multiline required onChange={handleDescricao}/>
+                            <TextField id="standard-basic" label="Contato" variant="standard" multiline required onChange={handleContato}/>
                             <div className="selecionar-categoria">
                                 <FormControl fullWidth>
                                     <InputLabel id="categoria-anuncio-2">Categoria</InputLabel>
                                     <Select
                                         labelId="categoria-anuncio-2"
                                         id="categoria-anuncio"
-                                        value={categoria}
                                         label="Categoria"
-                                        onChange={handleChangeCategoria}
+                                        onChange={handleCategoria}
                                         required
                                     >
                                         <MenuItem value={"carro"}>Autos e Peças</MenuItem>
@@ -53,7 +129,7 @@ const FormCriarAnuncio = () => {
                                     </Select>
                                 </FormControl>
                                 <div className="botao-criar-anuncio">
-                                    <Button variant="contained" color="success">
+                                    <Button variant="contained" color="success" onClick={handleSubmit}>
                                         Criar Anúncio 🥳
                                     </Button>
                                 </div>
